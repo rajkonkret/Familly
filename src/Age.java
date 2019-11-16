1d3c43e7d4f8b77db7a3cf35ddeb422db127b419
rajkonkret <rajkonkret660@gmail.com>
2019-11-16 11:41:42 +0100

    arra age cd

--- src/Age.java
+++ src/Age.java
@@ -1,7 +1,11 @@
 public class Age {
     public static void main(String[] args) {
         int shesAge = 28;
+        //declaration
         int[] ageArray;
+        //initialization
+        ageArray = new int[2];// dwa elementy w tablicy
+
 
 
     }
