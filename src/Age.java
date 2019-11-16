84416c1ff7dd695e0fcff7d9d4a1d349c1f18ad1
rajkonkret <rajkonkret660@gmail.com>
2019-11-16 11:47:09 +0100

    fori po age

--- src/Age.java
+++ src/Age.java
@@ -5,7 +5,12 @@ public class Age {
         int[] ageArray;
         //initialization
         ageArray = new int[2];// dwa elementy w tablicy
+        ageArray[0] = 28;
+        ageArray[1] = 29;
 
+        for (int i = 0; i < ageArray.length; i++) {
+            System.out.println(ageArray[i]);
+        }
 
 
     }
