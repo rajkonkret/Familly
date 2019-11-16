5e635783ab1b63754347967f8e5b82347a88c086
rajkonkret <rajkonkret660@gmail.com>
2019-11-16 11:33:54 +0100

    oldest dodano

--- src/AgeWithoutArrays.java
+++ src/AgeWithoutArrays.java
@@ -6,12 +6,16 @@ public class AgeWithoutArrays {
     public static void main(String[] args) {
         int shesAge = 28;
         int hesAge = 29;
+        int childAge = 1;
         //averrage
-        double average = (shesAge + hesAge) / 2.0;
+        double average = (shesAge + hesAge + childAge) / 3.0;
 
-        int youngest = Math.min(shesAge, hesAge);
+        int almostYoungest = Math.min(shesAge, hesAge);
+        int youngest = Math.min(almostYoungest, childAge);
 
+        int almostOldest = Math.max(shesAge, hesAge);
         int oldest = Math.max(shesAge, hesAge);
+
         System.out.println("Average: " + average);
         System.out.println("Youngest " + youngest);
         System.out.println("Oldest: " + oldest);
