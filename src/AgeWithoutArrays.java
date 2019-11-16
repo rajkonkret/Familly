cbc835331e9b6ba225b789b2170a0ea282e8f985
rajkonkret <rajkonkret660@gmail.com>
2019-11-16 11:28:26 +0100

    oldest dodano

--- src/AgeWithoutArrays.java
+++ src/AgeWithoutArrays.java
@@ -1,12 +1,20 @@
 import sun.plugin.extension.JavaExtensionInstaller;
 
+import java.util.Map;
+
 public class AgeWithoutArrays {
     public static void main(String[] args) {
         int shesAge = 28;
         int hesAge = 29;
         //averrage
         double average = (shesAge + hesAge) / 2.0;
+
+        int youngest = Math.min(shesAge, hesAge);
+
+        int oldest = Math.max(shesAge, hesAge);
         System.out.println("Average: " + average);
+        System.out.println("Youngest " + youngest);
+        System.out.println("Oldest: " + oldest);
 
     }
 }
