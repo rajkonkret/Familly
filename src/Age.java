dbcaaf90e8103fb537f62821134eff9dfacdf625
rajkonkret <rajkonkret660@gmail.com>
2019-11-16 22:29:58 +0100

    youngest w metodzie

--- src/Age.java
+++ src/Age.java
@@ -4,7 +4,7 @@ public class Age {
         //declaration
 
         //initialization
-        int[] ageArray = {28,29,1};
+        int[] ageArray = {28, 29, 1};
 
         int familyCount = ageArray.length;
 
@@ -16,16 +16,25 @@ public class Age {
 
         for (int age : ageArray) {
             sum += age;
-            youngest = Math.min(youngest, age);
+            youngest = youngest(ageArray,age);
             if (age > oldest) {
                 oldest = age;
             }
         }
 
-        double average = sum / familyCount;
-
-        System.out.println("Average = " + average);
+        double ave=average(ageArray,familyCount,sum);
+        System.out.println("Average = " + average(ageArray,familyCount,sum));
         System.out.println("Youngest = " + youngest);
         System.out.println("Oldest = " + oldest);
+        System.out.format((String.valueOf(ave)));
     }
+    public static double average(int[] ageArray, int familyCount, double sum) {
+        return  sum / familyCount;
+    }
+    public static int youngest(int[] ageArray, int age){
+        int youngest= ageArray[0];
+        youngest = Math.min(youngest, age);
+        return youngest;
+    }
+
 }
