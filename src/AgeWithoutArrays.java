e21a9e79f5e46ffd362fe0212e1eb5cb7836c020
rajkonkret <rajkonkret660@gmail.com>
2019-11-16 11:21:59 +0100

    pierwszy double

--- src/AgeWithoutArrays.java
+++ src/AgeWithoutArrays.java
@@ -1,6 +1,12 @@
+import sun.plugin.extension.JavaExtensionInstaller;
+
 public class AgeWithoutArrays {
     public static void main(String[] args) {
-
+        int shesAge = 28;
+        int hesAge = 29;
+        //averrage
+        double average = (shesAge + hesAge) / 2.0;
+        System.out.println("Average: " + average);
 
     }
 }
