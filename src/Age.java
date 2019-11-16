4148a336cd69359cce105c8a81380fabfd4b271f
rajkonkret <rajkonkret660@gmail.com>
2019-11-16 12:08:52 +0100

    max min

--- src/Age.java
+++ src/Age.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+
 public class Age {
     public static void main(String[] args) {
         int shesAge = 28;
@@ -14,8 +16,25 @@ public class Age {
             System.out.println(ageArray[i]);
             sum += ageArray[i];
         }
+
         double average = sum / familyCount;
+
+        int youngest = ageArray[0];
+        for (int i = 0; i < familyCount; i++) {
+            if (ageArray[i] < youngest) {
+                youngest = ageArray[i];
+            }
+        }
+
+        int oldest = ageArray[0];
+        for (int i = 0; i < familyCount; i++) {
+            if (ageArray[i] > oldest) {
+                oldest = ageArray[i];
+            }
+        }
         System.out.println("Average = " + average);
+        System.out.println("Youngest = " + youngest);
+        System.out.println("Oldest = " + oldest);
 
     }
 }
